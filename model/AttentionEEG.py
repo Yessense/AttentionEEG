05a2ab77f482899891243f27c1f0be7f30fd657f
yessense <akorchemnyj@gmail.com>
2022-07-14 08:36:00 +0300

    combined raw and fft

--- model/AttentionEEG.py
+++ model/AttentionEEG.py
@@ -70,22 +70,9 @@ class AttentionEEG(pl.LightningModule):
         self.f_sconv1d_3 = SConv1d(in_channels, in_channels, 3, 1, 1, bn=True, drop=drop)
         self.f_sconv1d_4 = SConv1d(in_channels, in_channels, 8, 2, 3, bn=True, drop=drop)
 
-        # Activations
-        self.activation = nn.ReLU()
-        self.final_activation = nn.Sigmoid()
-
         # temporal convs
         self.t_conv = nn.Conv1d(in_channels, self.temporal_convs, kernel_size=self.hidden_channels)
         self.t_bn = nn.BatchNorm1d(self.temporal_convs)
-
-        # temporal convs
-        self.fft_t_conv = nn.Conv1d(in_channels, self.temporal_convs, kernel_size=32)
-        self.fft_t_bn = nn.BatchNorm1d(self.temporal_convs)
-
-        # Person
-        self.p_lin1 = nn.Linear(self.temporal_convs, 128)
-        self.p_drop1 = nn.Dropout()
-        self.p_lin_class = nn.Linear(128, self.n_persons)
 
         # IM
         self.im_lin1 = nn.Linear(self.temporal_convs, 64)
@@ -95,20 +82,17 @@ class AttentionEEG(pl.LightningModule):
         self.accuracy = Accuracy()
         self.confusion_matrix = ConfusionMatrix(num_classes=self.n_classes)
 
-    def forward(self, raw, fft):
+        # Activations
+        self.activation = nn.ReLU()
+        self.final_activation = nn.Sigmoid()
+
+    def forward(self, raw):
         # Raw
         raw_out = self.r_sconv1d_1(raw)
         raw_out = self.r_sconv1d_2(raw_out)
         raw_out = self.r_sconv1d_3(raw_out)
         raw_out = self.r_sconv1d_4(raw_out)
         # raw_out -> (-1, 27, 64)
-
-        # fft
-        fft_out = self.f_sconv1d_1(fft)
-        fft_out = self.f_sconv1d_2(fft_out)
-        fft_out = self.f_sconv1d_3(fft_out)
-        fft_out = self.f_sconv1d_4(fft_out)
-        # fft_out -> (-1, 27, 32)
 
         raw_attention = raw_out @ raw_out.permute(0, 2, 1)
         raw_attention /= self.in_channels  # math.sqrt(self.in_channels)
@@ -124,40 +108,21 @@ class AttentionEEG(pl.LightningModule):
         # out -> (-1, 128)
         raw_out = self.t_bn(raw_out)
 
-        fft_attention = fft_out @ fft_out.permute(0, 2, 1)
-        fft_attention /= self.in_channels  # math.sqrt(self.in_channels)
-        fft_softmax = torch.softmax(fft_attention, dim=2)
-        # softmaxes -> (-1, 27, 27)
-
-        fft_out = fft_softmax @ fft_out
-        # out -> (-1, 27, 32)
-        fft_out = self.fft_t_conv(fft_out)
-        # out -> (-1, 128, 1)
-        fft_out = fft_out.view(-1, self.temporal_convs)
-        # out -> (-1, 128)
-        fft_out = self.fft_t_bn(fft_out)
-
-        person = self.p_drop1(self.activation(self.p_lin1(fft_out)))
-        person = self.p_lin_class(person)
-
         raw_out = self.im_drop1(self.activation(self.im_lin1(raw_out)))
         raw_out = self.im_lin_class(raw_out)
 
         raw_out = self.final_activation(raw_out)
-        person = self.final_activation(person)
+        # person = self.final_activation(person)
 
-        return raw_out, person
+        return raw_out
 
     def training_step(self, batch):
         raw_data, fft_data, target_im, target_person = batch
 
-        im_predicted, person_predicted = self.forward(raw_data, fft_data)
+        im_predicted = self.forward(raw_data)
 
         im_loss = self.loss_func(im_predicted, target_im)
         im_accuracy = self.accuracy(torch.argmax(im_predicted, dim=1), target_im)
-
-        person_loss = self.loss_func(person_predicted, target_person)
-        person_accuracy = self.accuracy(torch.argmax(person_predicted, dim=1), target_person)
 
         conf_matrix = self.confusion_matrix(torch.argmax(im_predicted, dim=1), target_im)
         conf_matrix = conf_matrix.cpu().detach().numpy()
@@ -168,11 +133,9 @@ class AttentionEEG(pl.LightningModule):
         # class_names = ['Rest', '1', 'Legs', '2']
 
         self.log("Train Loss", im_loss)
-        self.log("Person Loss", person_loss)
         self.log("Train Accuracy", im_accuracy, prog_bar=True)
-        self.log("Person Accuracy", person_accuracy)
 
-        return im_loss + 1 / 20 * person_loss
+        return im_loss
 
     def loss_func(self, y_pred, y_true):
         loss = nn.CrossEntropyLoss()
@@ -181,7 +144,7 @@ class AttentionEEG(pl.LightningModule):
     def validation_step(self, batch, idx, dataloader_idx):
         raw_data, fft_raw, im_target, person_target = batch
 
-        im_predicted, person_predicted = self.forward(raw_data, fft_raw)
+        im_predicted = self.forward(raw_data)
 
         loss = self.loss_func(im_predicted, im_target)
         accuracy = self.accuracy(im_predicted, im_target)
@@ -193,10 +156,6 @@ class AttentionEEG(pl.LightningModule):
         if dataloader_idx == 0:
             self.log("Val Loss", loss)
             self.log("Val Accuracy", accuracy, prog_bar=True)
-            person_loss = self.loss_func(person_predicted, person_target)
-            person_accuracy = self.accuracy(torch.argmax(person_predicted, dim=1), person_target)
-            self.log("Person Loss", person_loss)
-            self.log("Person Accuracy", person_accuracy)
             if idx % 50 == 0:
                 self.logger.experiment.log({'Matrix/Val Confusion Matrix': fig})
 
